bc9082fc8cced60f72940447bed6f0ed83b4bbb6
László Farkas <laszlo.farkas0@gmail.com>
2018-09-30 18:59:53 +0200

    connect to redis

---
+++ RESTService/src/main/java/com/flac/restservice/RestServiceConfiguration.java
@@ -0,0 +1,17 @@
+package com.flac.restservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+
+import java.util.concurrent.CountDownLatch;
+
+@Configuration
+public class RestServiceConfiguration {
+
+
+}
